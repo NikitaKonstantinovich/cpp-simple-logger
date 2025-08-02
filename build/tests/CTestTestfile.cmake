486fa0d92b2ab40cb69d80b343bd7aecae0c6682
Nikita Belyaev <nikitabelyaev0368@gmail.com>
2025-08-02 18:52:25 +0000

    chore: remove build directory from repository

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vboxuser/Documents/cpp-simple-logger/tests
-# Build directory: /home/vboxuser/Documents/cpp-simple-logger/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[logger_tests]=] "/home/vboxuser/Documents/cpp-simple-logger/build/tests/logger_tests")
-set_tests_properties([=[logger_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/vboxuser/Documents/cpp-simple-logger/tests/CMakeLists.txt;6;add_test;/home/vboxuser/Documents/cpp-simple-logger/tests/CMakeLists.txt;0;")
